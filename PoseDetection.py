30c3987c5dc468be07eef133ba2ab2660b6aa920
Mudit2003 <raimudit2003@gmail.com>
2024-07-15 19:52:32 +0530

    Next change

---
+++ PoseDetection.py
@@ -0,0 +1,21 @@
+import cv2
+
+def detectPosture(imagePath):
+    params = {
+        "model_folder": "openpose/models/",
+        "face": False,
+        "hand": False
+    }
+
+    # Starting OpenPose
+    opWrapper = op.WrapperPython()
+    opWrapper.configure(params)
+    opWrapper.start()
+
+    # Read image
+    image = cv2.imread(imagePath)
+    datum = op.Datum()
+    datum.cvInputData = image
+    opWrapper.emplaceAndPop([datum])
+
+    return datum.poseKeypoints
